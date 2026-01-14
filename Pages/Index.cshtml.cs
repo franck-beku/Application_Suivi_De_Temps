9cac8a3d19419c82483a87c9309a0bf9fb669ce8
William NTHIEKAM <william.nthiekam@ad.akto.fr>
2026-01-14 12:03:21 +0100

    Initialisation du projet Razor Pages avec Bootstrap

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Application_Suivi_De_Temps.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
